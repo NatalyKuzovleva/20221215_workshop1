9f2d506c2bdd11fb7c772ac23e431d79c0c54e37
Nataly <nt.kuzovleva@gmail.com>
2022-12-19 02:34:07 +0300

    Добавлен Task04

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -5,22 +5,39 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.WriteLine("Введите первое число:");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 != number2)
+// {
+//     if (number1 > number2)
+//     {
+//         Console.WriteLine($"Число {number1} - большее, а число {number2} - меньшее");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Число {number1} - меньшее, а число {number2} - большее");
+//     }
+// }
+// else
+//     {
+//         Console.WriteLine("Числа равны");
+//     }
+
 Console.WriteLine("Введите первое число:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 != number2)
+if (number1 > number2)
 {
-    if (number1 > number2)
-    {
-        Console.WriteLine($"Число {number1} - большее, а число {number2} - меньшее");
-    }
-    else
-    {
-        Console.WriteLine($"Число {number1} - меньшее, а число {number2} - большее");
-    }
+    Console.WriteLine($"Число {number1} - большее, а число {number2} - меньшее");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"Число {number1} - меньшее, а число {number2} - большее");
 }
 else
-    {
-        Console.WriteLine("Числа равны");
-    }
+{
+    Console.WriteLine("Числа равны");
+}
