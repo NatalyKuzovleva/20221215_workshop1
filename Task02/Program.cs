9a9b040e189dba3389fed19bba2fd02e0e096650
Nataly <nt.kuzovleva@gmail.com>
2022-12-19 02:28:39 +0300

    Решена задача в Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -4,3 +4,23 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 != number2)
+{
+    if (number1 > number2)
+    {
+        Console.WriteLine($"Число {number1} - большее, а число {number2} - меньшее");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number1} - меньшее, а число {number2} - большее");
+    }
+}
+else
+    {
+        Console.WriteLine("Числа равны");
+    }
