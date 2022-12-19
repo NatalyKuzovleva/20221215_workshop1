b67ed35cbf33cce0cc65c490ab7f8cb1f36b8507
Nataly <nt.kuzovleva@gmail.com>
2022-12-20 02:06:10 +0300

    Решена задача в Task07

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -6,3 +6,17 @@
 // 782 -> 2
 // 918 -> 8
 
+Console.WriteLine("Введите трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num > 99 && num < 1000)
+if (num >= 100 && num <= 999) // проверка на трехзначность: число лежит в диапазоне [100 ... 999]
+{
+    // Console.WriteLine(num % 10);     // 456 % 10 = 6   % - вычисления остатка от деления
+    // Console.WriteLine($"{num} -> {num % 10}");
+    Console.WriteLine($"Последняя цифра числа {num} = {num % 10}");
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число!");
+}
