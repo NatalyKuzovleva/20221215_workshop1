901d60145e2573ced9b5b8c55257dda94549c88d
Nataly <nt.kuzovleva@gmail.com>
2022-12-20 03:11:17 +0300

    В Task08 добавлен 3й вариант решения

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -26,28 +26,54 @@
 
 
 // Вариант 2
+// Console.WriteLine("Введите натуральное число:"); // натуральное число - положительное целое, не ноль
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number > 0)                         // проверка, является ли введенное число натуральным
+// {
+//     if (number == 1)                    // особая строка вывода, в случае ввода 1
+//     {
+//         Console.WriteLine($"{number} -> В диапазоне чисел от 1 до 1 нет чётных чисел.");
+//     }
+//     else
+//     {
+//         int counter = 2;                    // счётчик для цикла
+//         string res = "";                    // результат
+//         while (counter <= number)
+//         {
+//             res = res + counter + " ";      // конкатенация (сложение) строк
+//             counter = counter + 2;
+//         }
+//         Console.WriteLine($"{number} -> {res}");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Число не является натуральным!");
+// }
+
+
+
+// Вариант 3
 Console.WriteLine("Введите натуральное число:"); // натуральное число - положительное целое, не ноль
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number > 0)                         // проверка, является ли введенное число натуральным
+if (number <= 0)                         // проверка, является ли введенное число натуральным
 {
-    if (number == 1)                    // особая строка вывода, в случае ввода 1
-    {
-        Console.WriteLine($"{number} -> В диапазоне чисел от 1 до 1 нет чётных чисел.");
-    }
-    else
-    {
-        int counter = 2;                    // счётчик для цикла
-        string res = "";                    // результат
-        while (counter <= number)
-        {
-            res = res + counter + " ";      // конкатенация (сложение) строк
-            counter = counter + 2;
-        }
-        Console.WriteLine($"{number} -> {res}");
-    }
+    Console.WriteLine("Число не является натуральным!");
+}
+else if (number == 1)                    // особая строка вывода, в случае ввода 1
+{
+    Console.WriteLine($"{number} -> В диапазоне чисел от 1 до 1 нет чётных чисел.");
 }
 else
 {
-    Console.WriteLine("Число не является натуральным!");
+    int counter = 2;                    // счётчик для цикла
+    string res = "";                    // результат
+    while (counter <= number)
+    {
+        res = res + counter + " ";      // конкатенация (сложение) строк
+        counter = counter + 2;
+    }
+    Console.WriteLine($"{number} -> {res}");
 }
