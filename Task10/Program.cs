0c1d3931eaae3a2fefe18b3184accafeb85fe020
Nataly <nt.kuzovleva@gmail.com>
2022-12-25 01:36:25 +0300

    Решена задача в Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,3 +5,21 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number < 1000)
+{
+    int secondDigit = FindSecondDigit(number);
+    Console.WriteLine($"Вторая цифра трехзначного числа -> {secondDigit}");
+}
+else Console.WriteLine("Введено не трехзначное число!");
+
+
+int FindSecondDigit(int num)            // Метод для поиска 2й цифры трехзначного числа
+{
+    // int result = (num / 10) % 10;
+    // return result;
+    return (num / 10 % 10);
+}
