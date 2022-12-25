6c87f9e83d63d1cd16190fb72c1d407f2ceabf75
Nataly <nt.kuzovleva@gmail.com>
2022-12-26 01:12:11 +0300

    Решена задача в Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет. 
+// Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите целое число и нажмите Enter:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int CountDigits(int num)    // Метод, который вычисляет скольки-значное число
+// {
+//     if (num < 0)        // Если число отрицательное - делаем его положительным
+//     {
+//         num = num * (-1);
+//     }
+
+//     int counter = 1;
+//     while (num > 9)
+//     {
+//         num = num / 10;
+//         counter++;
+//     }
+//     return counter;
+// }
+
+// int quantity = CountDigits(number);
+// if (quantity == 1)
+// {
+//     Console.WriteLine($"Число состоит из {quantity} знака.");
+// }
+// else Console.WriteLine($"Число состоит из {quantity} знаков.");
+// //Console.WriteLine($"Число {quantity}-значное.");
+
+if (number > -100 && number < 100)              // Проверка, есть ли 3я цифра
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+
+else
+{
+    int thirdDigit = FindThirdDigit(number);
+    Console.WriteLine($"Третья цифра числа {number} -> {thirdDigit}");
+}
+
+int FindThirdDigit(int num)             // Метод по поиску 3-й цифры в числе
+{
+    if (num < 0)   num = num * (-1);     // Если число отрицательное - делаем его положительным
+
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+
+    int result = num % 10;
+    return result;
+}
