d158a13b58d5c1f4f63285aeb6816e5b1c695331
Nataly <nt.kuzovleva@gmail.com>
2022-12-26 01:33:34 +0300

    Решена задача в Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -5,3 +5,28 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите целое число от 1 до 7, затем нажмите ENTER:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 7 || day < 1)
+{
+    Console.WriteLine("Неправильно введён день недели!");
+}
+else
+{
+    if (CheckHoliday(day))
+    {
+        Console.WriteLine($"{day} -> Да");
+    }
+    else
+    {
+        Console.WriteLine($"{day} -> Нет");
+    }
+}
+
+bool CheckHoliday(int num)                  // Метод для проверки, является ли день выходным
+{
+    if (num == 6 || num == 7) return true;
+    else return false;
+}
