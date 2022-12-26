c8f6c4d2fa462e6c199da1c88fb45003351b06bb
Nataly <nt.kuzovleva@gmail.com>
2022-12-27 01:23:29 +0300

    Решена задача в Task17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -3,3 +3,28 @@
 // номер четверти плоскости, в которой находится эта
 // точка.
 
+Console.WriteLine("Введите координаты точки:");
+Console.Write("X = ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y = ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(x, y);
+string result = quarter > 0 
+            ? $"Указанные координаты соответствуют четверти -> {quarter}" 
+            : "Введены некорректные координаты. Координата не должны быть равна 0.";
+
+Console.WriteLine(result);            
+
+int Quarter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
+    return 0;   // return - завершает работу метода, поэтому тут нет else
+}
+
+
+// Процедура - это метод, который не возвращает значение (void), например, WriteLine
+// Функции -это методы, которые возвращают значение
