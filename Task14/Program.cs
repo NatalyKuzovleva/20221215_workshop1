b2dc2ed3081fa81ce8e39f9e468e6fc1ade2b465
Nataly <nt.kuzovleva@gmail.com>
2022-12-28 00:31:59 +0300

    Решена задача в Task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -3,4 +3,19 @@
 // одновременно 7 и 23.
 // 14 -> нет
 // 46 -> нет
-// 161 -> д
+// 161 -> да
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (CheckMultiplicity(number))
+{
+    Console.WriteLine($"{number} -> да, число кратно одновременно 7 и 23");
+}
+else Console.WriteLine($"{number} -> нет, число не кратно одновременно 7 и 23");
+
+bool CheckMultiplicity(int num)
+{
+    return (num % 7 == 0 && num % 23 == 0);
+}
+
