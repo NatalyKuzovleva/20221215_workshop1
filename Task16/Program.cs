7789c6df35830f6344d836d1e41176e53836bf03
Nataly <nt.kuzovleva@gmail.com>
2022-12-28 01:19:14 +0300

    Отредактирована задача в Task16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -11,13 +11,13 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(CheckSQR(number1, number2)
-? $"{number1}, {number2} -> да"
-: $"{number1}, {number2} -> нет");
+                ? $"{number1}, {number2} -> да"
+                : $"{number1}, {number2} -> нет");
 // if (CheckSQR(number1, number2)) Console.WriteLine($"{number1}, {number2} -> да");
 // else Console.WriteLine($"{number1}, {number2} -> нет");
 
 bool CheckSQR(int num1, int num2)
 {
-   // return num1 * num1 == num2 || num2 * num2 == num1;
+    // return num1 * num1 == num2 || num2 * num2 == num1;
     return num1 / num2 == num2 || num2 / num1 == num1;
 }
