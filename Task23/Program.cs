7b0e7f0b8ab15ba0ddbae99119095aedb0363cd0
Nataly <nt.kuzovleva@gmail.com>
2022-12-29 01:06:30 +0300

    Решена задача в Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -7,3 +7,24 @@
 // 3 | 27
 // 4 | 64
 // 5 | 125
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 1)          // Возврат на ввод числа, если оно не натуральное
+{
+    Console.WriteLine($"Введено не натуральное число! Введите положительное целое число:");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+CubeNum(number);
+
+void CubeNum(int num)
+{
+    int i = 1;
+    while (i <= num)
+    {
+        Console.WriteLine($"{i,3} | {i * i * i,7}"); // форматирование - задали длину строки 3 и 7 символов
+        i++;
+    }
+}
