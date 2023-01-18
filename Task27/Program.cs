6c7839e74c24a8920f7cf46ccc33e5aaba21886a
Nataly <nt.kuzovleva@gmail.com>
2023-01-19 01:59:08 +0300

    Решена задача в Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -5,3 +5,29 @@
 // 82 -> 10
 // 9012 -> 12
 
+
+int number = EnterNumberRequest("Введите число: ");     //  Запрос на ввод числа от пользователя
+int sum = DigitsSum(number);
+
+Console.WriteLine($"Сумма цифр в числе {number} = {sum}");
+
+/**********************  МЕТОДЫ  **********************/
+
+int EnterNumberRequest(string text)           // Метод на запрос ввода числа от пользователя
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int DigitsSum(int num)                      // Метод для вычисления суммы цифр в числе
+{
+    num = Math.Abs(num);    // модуль числа
+    int digitSum = 0;
+    while (num != 0)
+    {
+        digitSum = digitSum + num % 10;
+        num = num / 10;
+    }
+    return digitSum;
+}
