6b29ccb24325b0a169aac9b14b67f6689c9864b8
Nataly <nt.kuzovleva@gmail.com>
2023-01-19 00:55:36 +0300

    Решена задача в Task26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -4,4 +4,44 @@
 // 78 -> 2
 // 89126 -> 5
 
+int number = EnterNumberRequest("Введите число: ");     //  Запрос на ввод числа от пользователя
+int amount = DigitsAmount(number);
 
+Console.WriteLine($"Количество цифр в числе {number} = {amount}");
+
+
+/**********************  МЕТОДЫ  **********************/
+
+int EnterNumberRequest(string text)           // Метод на запрос ввода числа от пользователя
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int DigitsAmount(int num)                   // Метод для вычисления количества цифр в числе
+{
+    int count = 0;
+    if (num == 0) return count = 1;
+
+    while (num != 0)    //!=0 (пока не 0) на тот случай если пользователь ввел отрицательное значение
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+
+// int DigitsAmount(int num)                   // Метод для вычисления количества цифр в числе
+// {
+//     // if (num < 0) num = (-1) * num;
+//     num = Math.Abs(num);       // модуль числа
+//     int count = 1;
+//     while (num > 9)
+//     {
+//         num = num / 10;
+//         count++;
+//     }
+//     return count;
+// }
