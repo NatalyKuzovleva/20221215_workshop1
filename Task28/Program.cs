0814cdb3844093414b8d1c8b6d4ef01c3afb3e2a
Nataly <nt.kuzovleva@gmail.com>
2023-01-19 02:37:49 +0300

    Решена задача в Task28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -4,3 +4,31 @@
 // 4 -> 24
 // 5 -> 120
 
+int number = EnterNumberRequest("Введите число: ");     //  Запрос на ввод числа от пользователя
+
+if (number > 0)            // натуральное число - положительное целое, не ноль
+{
+    int factorial = ComputeFactorial(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
+}
+else Console.WriteLine("Введено не натуральное число! Число должно быть больше 0!");
+
+
+/**********************  МЕТОДЫ  **********************/
+
+int EnterNumberRequest(string text)        // Метод на запрос ввода числа от пользователя
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int ComputeFactorial(int num)             // Метод для вычисления факториала (произведение чисел от 1 до N)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result = result * i;            // Факториал числа N — это произведение натуральных чисел от 1 до N
+    }
+    return result;
+}
