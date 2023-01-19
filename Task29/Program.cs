79865f676e532b0d076e26890a84fd902a872d45
Nataly <nt.kuzovleva@gmail.com>
2023-01-19 03:23:39 +0300

    Решена задача в Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,3 +2,39 @@
 // из 8 элементов и выводит их на экран.
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+
+/**********************  МЕТОДЫ  **********************/
+
+void FillArray(int[] collection)            // Метод для заполнения элементов массива псевдослучайными числами
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(0, 100);
+    }
+}
+
+// void PrintArr(int[] collection)           // Метод для вывода массива в терминал
+// {
+//        for (int i = 0; i < collection.Length; i++)
+//     {
+//         Console.Write($"{collection[i]} ");
+
+//     }
+// }
+
+void PrintArray(int[] collection)           // Метод для вывода массива в терминал (на экран)
+{
+    Console.Write("[");
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (i < collection.Length - 1) Console.Write(collection[i] + ", ");
+        else Console.Write(collection[i]);
+    }
+
+    Console.Write("]");
+}
+
