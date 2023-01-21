cff940689a36145111b10c8160632e310b3c65e9
Nataly <nt.kuzovleva@gmail.com>
2023-01-22 01:28:57 +0300

    Решена задача в Task31

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -6,7 +6,10 @@
 // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
 // положительных чисел равна 29, сумма отрицательных равна -20.
 
+
+
 /**********************  МЕТОДЫ  **********************/
+
 int[] CreateArrayRndInt(int size, int min, int max)     // Создание массива, заполненного случайными числами
 // Метод создаёт массив
 // на вход принимает int size - размер массива, 
@@ -17,7 +20,6 @@ int[] CreateArrayRndInt(int size, int min, int max)     // Создание ма
     int[] array = new int[size];    // создаём массив
     Random rnd = new Random();      // создать объект Рандом - экземпляр класса Рандом
     // необходимо сначала создать объект рандом, потом использовать Next
-
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = rnd.Next(min, max + 1);
@@ -26,14 +28,46 @@ int[] CreateArrayRndInt(int size, int min, int max)     // Создание ма
 }
 
 
+void PrintArray(int[] array)      // Вывод массива на печать в формате [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
 
 
+int GetSumPositiveElem(int[] array)     // Метод находит сумму положительных элементов массива
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sum += array[i];
+    }
+    return sum;
+}
 
 
-// for (int i = 0; i < array.Length; i++)
-// {
-//     Console.Write("[");
-//     if (i < array.Length - 1) Console.Write(array[i] + ", ");
-//     else Console.Write(array[i]);
-//     Console.Write("]");
-// }
+int GetSumNegativeElem(int[] array)     // Метод находит сумму отрицательных элементов массива
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) sum += array[i];
+    }
+    return sum;
+}
+
+/*********************************************************************************/
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+
+int sumPositive = GetSumPositiveElem(arr);
+int sumNegative = GetSumNegativeElem(arr);
+
+Console.WriteLine($"Сумма положительных чисел равна {sumPositive}");
+Console.WriteLine($"Сумма отрицательных чисел равна {sumNegative}");
